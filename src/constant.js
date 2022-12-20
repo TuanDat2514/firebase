35946cef4ee4976cdeb8e4babc4909d2a15bd7b4
TuanDat <dat.trinh@adamosoft.com>
2022-12-20 08:05:24 +0700

    first commit

---
+++ src/constant.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB3Ink47n-bTLP2szfXK0q8ErRU0CgFzwE",
+  authDomain: "fire-client-7fc2a.firebaseapp.com",
+  projectId: "fire-client-7fc2a",
+  storageBucket: "fire-client-7fc2a.appspot.com",
+  messagingSenderId: "558036469518",
+  appId: "1:558036469518:web:6b721d5f95ba685b4a795f",
+};
